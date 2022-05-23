2b0109e7725c677890e4bb7f409774897999b746
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-23 17:57:25 +0200

    Update Task_0.7.py
    converted from farenheit to celsius as well

--- level_0_coding_challenges/Task_0.7.py
+++ level_0_coding_challenges/Task_0.7.py
@@ -1,5 +1,12 @@
+
 def convert_to_farenheit(celsius_degrees):
     farenheit = (celsius_degrees * 1.8) + 32
     return farenheit
+def convert_to_celsius(farenheit_degrees):
+    celsius_degrees = (farenheit_degrees - 32) * (5/9)
+    return celsius_degrees
 
-print("The degrees converted from celsius to farenheit are", convert_to_farenheit(25), "degrees")
+
+print(f"The degrees converted from farenheit to celcius are: {convert_to_celsius(77)})
+
+print(f"The degrees converted from celcius to farenheit are: {convert_to_farenheit(25)})
