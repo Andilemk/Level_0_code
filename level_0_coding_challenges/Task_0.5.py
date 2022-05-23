b0a28dff8e54b40f21ce700a72fafdad7326e2e7
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-23 17:30:16 +0200

    Update Task_0.5.py
    removed math module and used logic to find the square root

--- level_0_coding_challenges/Task_0.5.py
+++ level_0_coding_challenges/Task_0.5.py
@@ -1,12 +1,12 @@
-import math
 
-# Herons formula used the length of the sides of a triangle to calculate it's area
+
+
 
 def area_of_triangle(a,b,c):
     sides = a + b + c
     sides = 1/2*(sides)
     sides_perimeter = sides*(sides - a)*(sides - b)*(sides - c)
-    Area = math.sqrt(sides_perimeter)
+    Area = (sides_perimeter) ** 0.5 
     return Area
 
 print(area_of_triangle(3, 4, 5), "metres squared")
