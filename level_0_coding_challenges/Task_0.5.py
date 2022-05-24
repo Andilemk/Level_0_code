51f730c0679e35c26beab7c34a419cb0949461a0
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-24 07:44:05 +0200

    Update Task_0.5.py
    removed unnecessary spacing and removed and changed variable area to lower case

--- level_0_coding_challenges/Task_0.5.py
+++ level_0_coding_challenges/Task_0.5.py
@@ -1,13 +1,9 @@
-
-
-
-
 def area_of_triangle(a,b,c):
     sides = a + b + c
     sides = 1/2*(sides)
     sides_perimeter = sides*(sides - a)*(sides - b)*(sides - c)
-    Area = (sides_perimeter) ** 0.5 
-    return Area
+    area = (sides_perimeter) ** 0.5 
+    return area
 
 print(area_of_triangle(3, 4, 5), "metres squared")
     
