cc3cebd2247ea7fcd88998206b8075eeefde74d3
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-24 17:17:02 +0200

    Update Task_0.5.py
    gave length variables more meaningful names

--- level_0_coding_challenges/Task_0.5.py
+++ level_0_coding_challenges/Task_0.5.py
@@ -1,7 +1,7 @@
-def area_of_triangle(a,b,c):
-    sides = a + b + c
+def area_of_triangle(side_1,side_2,side_3):
+    sides = side_1 + side_2 + side_3
     sides = 1/2*(sides)
-    sides_perimeter = sides*(sides - a)*(sides - b)*(sides - c)
+    sides_perimeter = sides*(sides - side_1)*(sides - side_2)*(sides - side_3)
     area = (sides_perimeter) ** 0.5 
     return area
 
