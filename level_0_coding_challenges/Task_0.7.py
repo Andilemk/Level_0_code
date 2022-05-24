993ef056d332409f66e0ad321697418e460b5c57
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-24 17:23:38 +0200

    Update Task_0.7.py
    corrected spelling error

--- level_0_coding_challenges/Task_0.7.py
+++ level_0_coding_challenges/Task_0.7.py
@@ -1,4 +1,3 @@
-
 def convert_to_farenheit(celsius_degrees):
     farenheit = (celsius_degrees * 1.8) + 32
     return farenheit
@@ -6,7 +5,6 @@ def convert_to_celsius(farenheit_degrees):
     celsius_degrees = (farenheit_degrees - 32) * (5/9)
     return celsius_degrees
 
+print(f"The degrees converted from farenheit to celsius are: {convert_to_celsius(77)}")
 
-print(f"The degrees converted from farenheit to celcius are: {convert_to_celsius(77)}")
-
-print(f"The degrees converted from celcius to farenheit are: {convert_to_farenheit(25)}")
+print(f"The degrees converted from celsius to farenheit are: {convert_to_farenheit(25)}")
