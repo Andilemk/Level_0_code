61fbdb913f7ef2cddf042f65a6e2f7ace267b158
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-24 17:27:18 +0200

    Update Task_0.8.py
    resolved error on line 6

--- level_0_coding_challenges/Task_0.8.py
+++ level_0_coding_challenges/Task_0.8.py
@@ -3,7 +3,7 @@ def change_to_time(number):
     minutes = number % 60
     if minutes < 10:
         minutes = "0" + str(minutes)
-    if hours =< 1:
+    if hours == 1:
         print(f"the time is {hours} hour and {minutes} minutes")
     else:
         print(f"the time is {hours} hours and {minutes} minutes")
