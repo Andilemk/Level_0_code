295f02b6ce6ad0034262b567c1d85c583b1335c9
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-25 15:00:48 +0200

    Update task_0_10.py
    changed the code to account for case and repeated common  letters

--- level_0_coding_challenges/task_0_10.py
+++ level_0_coding_challenges/task_0_10.py
@@ -1,10 +1,12 @@
 def common_letters(string1, string2):
     common_char = ''
+    string1 = string1.lower()
+    string2 = string2.lower()
     for letter in string2:
-        if letter in string1:
+        if letter in string1 and letter not in common_char:
             common_char += letter + ","
             
     print(f"Common letters: {common_char}")
     
-common_letters("Njabulo", "Khumalo")
+common_letters("Njabulo", "nnJhumalo")
             
