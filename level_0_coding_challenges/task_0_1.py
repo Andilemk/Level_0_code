9972caaa6209bf7f0e3c0274010427a089d03d21
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-25 13:28:57 +0200

    Update task_0_1.py

--- level_0_coding_challenges/task_0_1.py
+++ level_0_coding_challenges/task_0_1.py
@@ -1,8 +1,8 @@
 x = 0
 y = 1
-print(f"{x}")
-print(f"{y}")
+print(f"The value of x before the calculation = {x}")
+print(f"The value of y before the calculation = {y}")
 x = x + 3
 y = y + x
-print(f"the value of x = {x}")
-print(f"the value of x = {y}")
+print(f"the value of x after the calculation = {x}")
+print(f"the value of x after the calculation = {y}")
