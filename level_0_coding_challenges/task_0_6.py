b96035ffc8ac392fd2154ad7f016cfee86134c8c
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-25 19:14:49 +0200

    Update task_0_6.py
    changed function to three arguments

--- level_0_coding_challenges/task_0_6.py
+++ level_0_coding_challenges/task_0_6.py
@@ -1,9 +1,9 @@
-def maximum_value(num1, num2, num3, num4):
+def maximum_value(num1, num2, num3):
     largest_num = 0 
-    for i in num1,num2,num3,num4:
+    for i in num1,num2,num3:
         if i > largest_num:
             largest_num = i
     return largest_num
     
-print(maximum_value(29,4,25,29))
+print(maximum_value(29,4,25))
     
