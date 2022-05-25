083d350a0faada710304e23279239fef9dd10715
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-25 19:39:24 +0200

    Update task_0_10.py
    removed unwanted comma at the end.

--- level_0_coding_challenges/task_0_10.py
+++ level_0_coding_challenges/task_0_10.py
@@ -5,7 +5,7 @@ def common_letters(string1, string2):
     for letter in string2:
         if letter in string1 and letter not in common_char:
             common_char += letter + ","
-            
+    common_char = common_char.rstrip(",")
     print(f"Common letters: {common_char}")
     
 common_letters("Njabulo", "nnJhumalo")
