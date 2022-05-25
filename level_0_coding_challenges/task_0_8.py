77ffc2dcb2bf16c3a12c42bb592b7fe2112b6f82
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-25 14:42:28 +0200

    Update task_0_8.py
    changed the code to accommodate singular and plurals

--- level_0_coding_challenges/task_0_8.py
+++ level_0_coding_challenges/task_0_8.py
@@ -3,10 +3,14 @@ def change_to_time(number):
     minutes = number % 60
     if minutes < 10:
         minutes = "0" + str(minutes)
-    if hours == 1:
+    if hours == 1 and minutes == "01":
+        print(f"the time is {hours} hour and {minutes} minute")
+    elif hours == 1:
         print(f"the time is {hours} hour and {minutes} minutes")
+    elif minutes == "01":
+         print(f"the time is {hours} hours and {minutes} minute")
     else:
         print(f"the time is {hours} hours and {minutes} minutes")
-        
     
-change_to_time(60)
+change_to_time(62)
+
