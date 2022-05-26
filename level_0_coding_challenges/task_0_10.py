7b43f415a12b6a499cc52462a74e3f03c5b9eb40
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-26 22:24:40 +0200

    Update task_0_10.py
    added spacing between commas

--- level_0_coding_challenges/task_0_10.py
+++ level_0_coding_challenges/task_0_10.py
@@ -4,8 +4,8 @@ def common_letters(string1, string2):
     string2 = string2.lower()
     for letter in string2:
         if letter in string1 and letter not in common_char:
-            common_char += letter + ","
-    common_char = common_char.rstrip(",")
+            common_char += letter + ", "
+    common_char = common_char.rstrip(", ")
     print(f"Common letters: {common_char}")
     
 common_letters("Njabulo", "nnJhumalo")
