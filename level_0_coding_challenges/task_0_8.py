7f008e36a66d8b00e856bc41fd9f6bb5bed50a86
Andilemk <68894553+Andilemk@users.noreply.github.com>
2022-05-27 10:00:19 +0200

    Update task_0_8.py
    changed and to "," in f strings

--- level_0_coding_challenges/task_0_8.py
+++ level_0_coding_challenges/task_0_8.py
@@ -4,13 +4,13 @@ def change_to_time(number):
     if minutes < 10:
         minutes = "0" + str(minutes)
     if hours == 1 and minutes == "01":
-        print(f"the time is {hours} hour and {minutes} minute")
+        print(f"the time is {hours} hour, {minutes} minute")
     elif hours == 1:
-        print(f"the time is {hours} hour and {minutes} minutes")
+        print(f"the time is {hours} hour, {minutes} minutes")
     elif minutes == "01":
-         print(f"the time is {hours} hours and {minutes} minute")
+         print(f"the time is {hours} hours, {minutes} minute")
     else:
-        print(f"the time is {hours} hours and {minutes} minutes")
+        print(f"the time is {hours} hours, {minutes} minutes")
     
-change_to_time(62)
+change_to_time(61)
 
